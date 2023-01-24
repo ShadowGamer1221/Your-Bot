ad02eac5aa943a47276cf585fd98aa4cbba87083
ShadowGamer1221 <85442315+ShadowGamer1221@users.noreply.github.com>
2023-01-24 20:44:32 +0100

    Update info.ts

--- src/commands/information/info.ts
+++ src/commands/information/info.ts
@@ -27,7 +27,7 @@ class InfoCommand extends Command {
                     description: 'Who do you want to view the information of?',
                     autocomplete: true,
                     required: false,
-                    type: 'String',
+                    type: 'DiscordUser',
                 },
             ],
             permissions: [
@@ -42,8 +42,40 @@ class InfoCommand extends Command {
 
     async run(ctx: CommandContext) {
         if(!config.database.enabled) return ctx.reply({ embeds: [ getNoDatabaseEmbed() ] });
+        
+    
 
         let robloxUser: User | PartialUser;
+
+        if(ctx.args['roblox-user']) {
+            let member = ctx.args['roblox-user']
+            member = member.replace("<","");
+            member = member.replace("@","");
+            member = member.replace("!","");
+            member = member.replace(">","");
+    
+            const guild = ctx.guild
+      
+            const realMember = await guild.members.fetch(member)
+
+            try {
+                const secondrobloxUser = await getLinkedRobloxUser(realMember.id, ctx.guild.id)
+                robloxUser = secondrobloxUser[0];
+            } catch (err) {
+                try {
+                    const idQuery = ctx.args['roblox-user'].replace(/[^0-9]/gm, '');
+                    const discordUser = await discordClient.users.fetch(idQuery);
+                    const linkedUser = await getLinkedRobloxUser(discordUser.id, ctx.guild.id);
+                    if(!linkedUser) throw new Error();
+                    robloxUser = linkedUser;
+                } catch (err) {
+                    return ctx.reply({ embeds: [ getInvalidRobloxUserEmbed() ]});
+                }
+            }
+            
+        console.log(member)}
+
+
         try {
             if(ctx.args['roblox-user']) {
                 robloxUser = await robloxClient.getUser(ctx.args['roblox-user'] as number);
